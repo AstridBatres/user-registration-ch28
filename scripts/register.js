1fb303cb55479220181f554997a5c5d77a90b563
AstridBatres-Guerrero <astridcbg28@gmail.com>
2022-04-26 21:22:18 -0700

    Revised 1st assignment
    105-first class

--- scripts/register.js
+++ scripts/register.js
@@ -1,26 +1,33 @@
 //console.log("Script");
 
 //create a user and display it on console
-function User(first, last,email,password,gender,age){
+function User(first,last,email,password,gender,age,adress,phone,payment){
         this.first=first;
         this.last=last;
         this.email=email;
         this.password=password;
         this.gender=gender;
         this.age=age;
+        this.adress=adress;
+        this.phone=phone;
+        this.payment=payment;
 }
 
 function register(){
-        let inputfirst= $("#txtfirst").val;//getting value
-        let inputlast= $("#txtlast").val;
-        let inputemail =$("#txtemail").val;
-        let inputpassword =$("#txtpassword");
-        let inputgender=$("#txtgender").val;
-        let inputage=$("#txtage").val;
+        let inputfirst= $("#txtfirst").val();//getting value
+        let inputlast= $("#txtlast").val();
+        let inputemail =$("#txtemail").val();
+        let inputpassword =$("#txtpassword").val();
+        let inputgender=$("#txtgender").val();
+        let inputage=$("#txtage").val();
+        let inputadress=$("#txtadress").val();
+        let inputphone=$("#txtphone").val();
+        let inputpayment=$("#txtpayment").val();
 
-        console.log(inputfirst,inputlast,inputemail,inputpassword,inputgender,inputage);
+        //console.log(inputfirst,inputlast,inputemail,inputpassword,inputgender,inputage,inputadress,inputphone,inputpayment);
 
-        let newUser = new User(inputfirst,inputlast,inputemail,inputpassword,inputgender,inputage);
+        let newUser = new User(inputfirst,inputlast,inputemail,inputpassword,inputgender, inputage,inputadress,inputphone,inputpayment);
+        console.log(newUser)
 }
 
 function init(){
